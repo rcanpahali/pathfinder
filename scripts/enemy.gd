3dfc9108a86acdf115d567c31d862be8057eab00
R.Can Pahalı <canpahali@gmail.com>
2024-05-18 20:58:49 +0200

    initial commit

---
+++ scripts/enemy.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player_group")
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+@export var base_movement_speed: float = 30.0
+@export var movement_speed_variation: float = 10.0
+var movement_speed: float
+
+func _ready() -> void:
+	# Assign a random movement speed within the variation range
+	movement_speed = base_movement_speed + randf_range(-movement_speed_variation, movement_speed_variation)
+
+func _physics_process(_delta) -> void:
+	move_to_player_position()
+
+func move_to_player_position() -> void:
+	var direction: Vector2 = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
+	update_animation(direction)
+
+func update_animation(direction: Vector2) -> void:
+	if direction.x < 0:
+		animated_sprite_2d.flip_h = true
+	elif direction.x > 0:
+		animated_sprite_2d.flip_h = false
---
+++ scripts/player.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+#@onready var animation_tree: AnimationTree = $AnimationTree
+#@onready var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+#@export var starting_direction: Vector2 = Vector2(0,1)
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var SPEED: float = 200.0
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+	
+	handle_movement()
+
+func handle_movement() -> void:
+		var input_direction: Vector2 = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up"))
+		
+		velocity = input_direction.normalized() * SPEED
+		move_and_slide()
+		updateAnimation(input_direction)
+
+func updateAnimation(input_direction: Vector2) -> void:
+	#switch between states: running <--> idle
+	if(velocity == Vector2.ZERO):
+		animated_sprite_2d.animation = "idle";
+	else:
+		animated_sprite_2d.animation = "walk";
+	
+	# look left-right
+	if(input_direction.x < 0):
+		animated_sprite_2d.flip_h = true;
+	elif(input_direction.x > 0):
+		animated_sprite_2d.flip_h = false;
+	else: 
+		return;
+	
+#func update_animation_position_parameters(move_input: Vector2) -> void:
+	#if move_input != Vector2.ZERO:
+		#animation_tree.set("parameters/Walk/blend_position", move_input)
+		#animation_tree.set("parameters/Idle/blend_position", move_input)
+
+#func update_animation_state_machine(move_input: Vector2) -> void:
+	#if move_input != Vector2.ZERO:
+		#state_machine.travel("Walk") #Switch between state machine  definitions
+	#else:
+		#state_machine.travel("Idle")
